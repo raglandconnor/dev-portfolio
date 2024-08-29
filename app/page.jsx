9a405144534bd14b1b2d815e6dbc8fd694bc655f
raglandconnor <raglandconnor@ufl.edu>
2024-08-29 19:14:16 -0400

    Initialize shadcn UI, add .env to gitignore

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import { CardDemo } from './components/cardDemo';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <CardDemo />
+    </main>
+  );
+}
