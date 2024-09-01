94c9e0889cbabbea1936560f2aa1995a38e8ddb6
raglandconnor <raglandconnor@ufl.edu>
2024-08-31 21:37:57 -0400

    Create profile about section layout

--- app/users/[userId]/page.jsx
+++ app/users/[userId]/page.jsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+import BioCard from '../components/BioCard';
+
 const ProfilePage = ({ params }) => {
   return (
-    <div className="flex flex-col items-center mt-24">
-      This is the userId: {params.userId}
-    </div>
+    <main className="flex flex-col items-center mt-8 md:mt-24">
+      <div className="gap-3 md:flex md:flex-row md:gap-3 w-[95%] lg:w-[1000px] p-5 border rounded-2xl">
+        <Image
+          src="/next.svg"
+          width={0}
+          height={0}
+          className="w-96 h-96 rounded-2xl border inline-block"
+        />
+        <BioCard userId={params.userId} />
+      </div>
+    </main>
   );
 };
 
---
+++ app/users/components/BioCard.jsx
@@ -0,0 +1,37 @@
+import { Linkedin, Github, Link as LinkIcon } from 'lucide-react';
+import Link from 'next/link';
+
+const BioCard = ({ userId }) => {
+  return (
+    <div className="inline-block mt-3 md:mt-0 w-full border rounded-2xl">
+      <div className="py-2 px-3">
+        <h1 className="text-4xl md:text-6xl font-bold">{userId}</h1>
+        <h2 className="text-xl">Senior Software Engineer</h2>
+        <div className="flex flex-row gap-2 mt-1">
+          <Link href="/">
+            <Linkedin size={36} className="bg-gray-200 p-1 rounded-md" />
+          </Link>
+          <Link href="/">
+            <Github size={36} className="bg-gray-200 p-1 rounded-md" />
+          </Link>
+          <Link href="/">
+            <LinkIcon size={36} className="bg-gray-200 p-1 rounded-md" />
+          </Link>
+        </div>
+        <div className="mt-4">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
+            assumenda architecto dolorum magni neque. Soluta labore quia
+            possimus enim? Sed fugiat maxime aliquid quae sit dicta. Ipsam ex
+            eius non. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Sint assumenda architecto dolorum magni neque. Soluta labore quia
+            possimus enim? Sed fugiat maxime aliquid quae sit dicta. Ipsam ex
+            eius non.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BioCard;
