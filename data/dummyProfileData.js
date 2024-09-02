eb031ff707517747d94199cdc1e1a2fb3c3d9def
raglandconnor <raglandconnor@ufl.edu>
2024-09-02 11:56:16 -0400

    Dynamically display bio card based on dummy data

---
+++ data/dummyProfileData.js
@@ -0,0 +1,128 @@
+export const dummyUserData = [
+  {
+    username: 'raglandconnor',
+    displayName: 'Connor Ragland',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/1?v=4',
+    currentPosition: 'Computer Science and Mathematics Student',
+    location: 'Gainesville, FL',
+    email: 'connor@rglnd.com',
+    githubUsername: 'raglandconnor',
+    linkedInUsername: 'raglandconnor',
+    website: 'https://raglandconnor.com',
+    bio: `I'm a Computer Science and Mathematics student at the University of Florida. `,
+
+    experience: [
+      {
+        title: 'Software Engineer Intern',
+        company: 'Facebook',
+        startDate: 'May 2021',
+        endDate: 'August 2021',
+        location: 'Menlo Park, CA',
+        description: `I worked on the React team.`,
+      },
+      {
+        title: 'Software Engineer Intern',
+        company: 'Google',
+        startDate: 'May 2020',
+        endDate: 'August 2020',
+        location: 'Mountain View, CA',
+        description: `I worked on the Angular team.`,
+      },
+    ],
+    education: [
+      {
+        school: 'University of Florida',
+        degree: 'Bachelor of Science',
+        fieldOfStudy: 'Computer Science',
+        startDate: 'August 2018',
+        endDate: 'May 2022',
+        location: 'Gainesville, FL',
+      },
+    ],
+    projects: [
+      {
+        title: 'Project 1',
+        description: `This is a project I worked on.`,
+        links: {
+          github: 'https://github.com/raglandconnor',
+          website: 'https://raglandconnor.com',
+          readMore: 'https://www.google.com/',
+          youtube: 'https://www.youtube.com/',
+        },
+      },
+      {
+        title: 'Project 2',
+        description: `This is a project I worked on 2.`,
+        links: {
+          github: 'https://github.com/raglandconnor',
+          website: 'https://raglandconnor.com',
+          readMore: 'https://www.google.com/',
+          youtube: 'https://www.youtube.com/',
+        },
+      },
+    ],
+  },
+  {
+    username: 'raglandconnor1',
+    displayName: 'Connor Ragland',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/1?v=4',
+    currentPosition: 'Computer Science and Mathematics Student1111111',
+    location: 'Gainesville, FL',
+    email: 'connor@rglnd.com',
+    githubUsername: 'raglandconnor',
+    linkedInUsername: 'raglandconnor',
+    website: 'https://raglandconnor.com',
+    bio: `I'm a Computer Science and Mathematics student at the University of Florida. 11111111`,
+
+    experience: [
+      {
+        title: 'Software Engineer Intern',
+        company: 'Facebook',
+        startDate: 'May 2021',
+        endDate: 'August 2021',
+        location: 'Menlo Park, CA',
+        description: `I worked on the React t1111eam.`,
+      },
+      {
+        title: 'Software Engineer Intern',
+        company: 'Google',
+        startDate: 'May 2020',
+        endDate: 'August 2020',
+        location: 'Mountain View, CA',
+        description: `I worked on the1111 Angular team.`,
+      },
+    ],
+    education: [
+      {
+        school: 'University o111f Florida',
+        degree: 'Bachelor of Science',
+        fieldOfStudy: 'Comput111er Science',
+        startDate: 'August 2018',
+        endDate: 'May 2022',
+        location: 'Gainesville, FL',
+      },
+    ],
+    projects: [
+      {
+        title: 'Pr111oject 1',
+        description: `This is 111a project I worked on.`,
+        links: {
+          github: 'https://github.com/raglandconnor',
+          website: 'https://raglandconnor.com',
+          readMore: 'https://www.google.com/',
+          youtube: 'https://www.youtube.com/',
+        },
+      },
+      {
+        title: 'Project 2',
+        description: `This is a1111 project I worked on 2.`,
+        links: {
+          github: 'https://github.com/raglandconnor',
+          website: 'https://raglandconnor.com',
+          readMore: 'https://www.google.com/',
+          youtube: 'https://www.youtube.com/',
+        },
+      },
+    ],
+  },
+];
